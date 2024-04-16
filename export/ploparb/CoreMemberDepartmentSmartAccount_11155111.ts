8823b11486994746f7e2c1d3982bb3f62a880358
Plopmenz <plopmenz@gmail.com>
2024-04-16 13:07:39 +0200

    formatting

--- export/ploparb/CoreMemberDepartmentSmartAccount_11155111.ts
+++ export/ploparb/CoreMemberDepartmentSmartAccount_11155111.ts
@@ -1,1 +1,62 @@
-export const CoreMemberDepartmentSmartAccount_11155111Contract = {"address":"0xB7f7315088EA8E0CFb7b2a46Ca6ca18ac1370A79","abi":[{"type":"constructor","inputs":[],"stateMutability":"nonpayable"},{"type":"fallback","stateMutability":"payable"},{"type":"receive","stateMutability":"payable"},{"type":"function","name":"multicall","inputs":[{"name":"data","type":"bytes[]","internalType":"bytes[]"}],"outputs":[{"name":"results","type":"bytes[]","internalType":"bytes[]"}],"stateMutability":"nonpayable"},{"type":"function","name":"performCall","inputs":[{"name":"to","type":"address","internalType":"address"},{"name":"value","type":"uint256","internalType":"uint256"},{"name":"data","type":"bytes","internalType":"bytes"}],"outputs":[{"name":"returnValue","type":"bytes","internalType":"bytes"}],"stateMutability":"nonpayable"},{"type":"function","name":"performDelegateCall","inputs":[{"name":"to","type":"address","internalType":"address"},{"name":"data","type":"bytes","internalType":"bytes"}],"outputs":[{"name":"returnValue","type":"bytes","internalType":"bytes"}],"stateMutability":"nonpayable"},{"type":"error","name":"AddressEmptyCode","inputs":[{"name":"target","type":"address","internalType":"address"}]},{"type":"error","name":"AddressInsufficientBalance","inputs":[{"name":"account","type":"address","internalType":"address"}]},{"type":"error","name":"FailedInnerCall","inputs":[]},{"type":"error","name":"FunctionNotFound","inputs":[{"name":"functionSelector","type":"bytes4","internalType":"bytes4"}]},{"type":"error","name":"NotOwner","inputs":[{"name":"account","type":"address","internalType":"address"},{"name":"owner","type":"address","internalType":"address"}]}]} as const;
+export const CoreMemberDepartmentSmartAccount_11155111Contract = {
+  address: "0xB7f7315088EA8E0CFb7b2a46Ca6ca18ac1370A79",
+  abi: [
+    { type: "constructor", inputs: [], stateMutability: "nonpayable" },
+    { type: "fallback", stateMutability: "payable" },
+    { type: "receive", stateMutability: "payable" },
+    {
+      type: "function",
+      name: "multicall",
+      inputs: [{ name: "data", type: "bytes[]", internalType: "bytes[]" }],
+      outputs: [{ name: "results", type: "bytes[]", internalType: "bytes[]" }],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "function",
+      name: "performCall",
+      inputs: [
+        { name: "to", type: "address", internalType: "address" },
+        { name: "value", type: "uint256", internalType: "uint256" },
+        { name: "data", type: "bytes", internalType: "bytes" },
+      ],
+      outputs: [{ name: "returnValue", type: "bytes", internalType: "bytes" }],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "function",
+      name: "performDelegateCall",
+      inputs: [
+        { name: "to", type: "address", internalType: "address" },
+        { name: "data", type: "bytes", internalType: "bytes" },
+      ],
+      outputs: [{ name: "returnValue", type: "bytes", internalType: "bytes" }],
+      stateMutability: "nonpayable",
+    },
+    {
+      type: "error",
+      name: "AddressEmptyCode",
+      inputs: [{ name: "target", type: "address", internalType: "address" }],
+    },
+    {
+      type: "error",
+      name: "AddressInsufficientBalance",
+      inputs: [{ name: "account", type: "address", internalType: "address" }],
+    },
+    { type: "error", name: "FailedInnerCall", inputs: [] },
+    {
+      type: "error",
+      name: "FunctionNotFound",
+      inputs: [
+        { name: "functionSelector", type: "bytes4", internalType: "bytes4" },
+      ],
+    },
+    {
+      type: "error",
+      name: "NotOwner",
+      inputs: [
+        { name: "account", type: "address", internalType: "address" },
+        { name: "owner", type: "address", internalType: "address" },
+      ],
+    },
+  ],
+} as const;
